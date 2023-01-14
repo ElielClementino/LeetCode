5be378bfaa2e47ec8623214aa70fdd9f26b330dd
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2023-01-13 23:25:05 -0300

    Coloca print na função

--- easy/ConvertTemperature.py
+++ easy/ConvertTemperature.py
@@ -1,6 +1,9 @@
 # https://leetcode.com/problems/convert-the-temperature/description/
 from typing import List
+celsius = 36.50
 def convertTemperature(celsius: float) -> List[float]:
     kelvin = celsius + 273.15
     fahrenheit = celsius * 1.80 + 32.00
     return [kelvin, fahrenheit]
+
+print(convertTemperature(celsius))
